7b0c605ebbf09add0c09266706b811292ebf38ea
sinchana <sinchana@gmail.com>
2021-12-06 11:46:12 +0530

    first commit

---
+++ src/test/java/com/sinchu/docker_jenkins_integration_sample/AppTest.java
@@ -0,0 +1,20 @@
+package com.sinchu.docker_jenkins_integration_sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
